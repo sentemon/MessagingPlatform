87be99bb5f5d7699f96425cb2c9816f54069d257
Ivan Sentemon <sentemon.i@gmail.com>
2024-07-31 13:06:19 +0200

    Add AppDbContext

---
+++ src/MessagingPlatform.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+using MessagingPlatform.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MessagingPlatform.Infrastructure.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<DbContext> options) : base(options)
+    {
+        
+    }
+}
