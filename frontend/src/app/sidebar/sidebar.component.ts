0a4af472d76c42c78b45c35ead2c633704bab652
Ivan Sentemon <sentemon.i@gmail.com>
2024-08-27 14:07:55 +0200

    add example users to side bar

--- frontend/src/app/sidebar/sidebar.component.ts
+++ frontend/src/app/sidebar/sidebar.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import {User} from "../models/user";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
-  
+  users: User[] = [
+      {firstName: "Ivan", lastName: "Sentemon", avatar: "avatar.svg"},
+      {firstName: "Nom", lastName: "Nometnes", avatar: "avatar.svg"},
+
+  ]
 }
