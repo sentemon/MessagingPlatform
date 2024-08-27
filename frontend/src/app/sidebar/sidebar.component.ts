b2ee54c3620e78804ca0c6559f6fc192d426c117
Ivan Sentemon <sentemon.i@gmail.com>
2024-08-27 14:15:41 +0200

    Add sticky position to sidebar header

--- frontend/src/app/sidebar/sidebar.component.ts
+++ frontend/src/app/sidebar/sidebar.component.ts
@@ -13,6 +13,19 @@ export class SidebarComponent {
   users: User[] = [
       {firstName: "Ivan", lastName: "Sentemon", avatar: "avatar.svg"},
       {firstName: "Nom", lastName: "Nometnes", avatar: "avatar.svg"},
-
+      {firstName: "Jane", lastName: "January", avatar: "avatar.svg"},
+      {firstName: "Female", lastName: "Female", avatar: "avatar.svg"},
+      {firstName: "Male", lastName: "Male", avatar: "avatar.svg"},
+      {firstName: "John", lastName: "John", avatar: "avatar.svg"},
+      {firstName: "Peter", lastName: "Parker", avatar: "avatar.svg"},
+      {firstName: "Roman", lastName: "Roman", avatar: "avatar.svg"},
+      {firstName: "Sain", lastName: "Sain", avatar: "avatar.svg"},
+      {firstName: "Gol", lastName: "Med", avatar: "avatar.svg"},
+      {firstName: "Eric", lastName: "Eric", avatar: "avatar.svg"},
+      {firstName: "French", lastName: "French", avatar: "avatar.svg"},
+      {firstName: "Chico", lastName: "Lachowski", avatar: "avatar.svg"},
+      {firstName: "Marek", lastName: "Male", avatar: "avatar.svg"},
+      {firstName: "John", lastName: "John", avatar: "avatar.svg"},
+      {firstName: "Alley", lastName: "Downey", avatar: "avatar.svg"},
   ]
 }
