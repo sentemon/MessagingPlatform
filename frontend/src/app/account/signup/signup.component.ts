dd779f7a36e50d4074636086e3d0c2e459bd4279
Ivan Sentemon <sentemon.i@gmail.com>
2024-08-28 15:27:05 +0200

    update signup component

--- frontend/src/app/account/signup/signup.component.ts
+++ frontend/src/app/account/signup/signup.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import {SignUpUser} from "../../models/signupuser";
+import {AuthService} from "../../services/auth.service";
+import {Router} from "@angular/router";
+import {NgIf} from "@angular/common";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf,
+    FormsModule
+  ],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignUpComponent {
+  signUpUser = new SignUpUser();
 
+  constructor(private authService: AuthService, private router: Router) { }
+
+  signUp(user: SignUpUser) {
+    this.authService.signUp(user).subscribe((str: string) => {
+      console.log(str);
+      this.router.navigate(['/']);
+    })
+  }
+
+  protected readonly SignUpUser = SignUpUser;
 }
