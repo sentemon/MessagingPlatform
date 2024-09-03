528d6ff909f23fc133825d94bb1d60432119bfd3
Ivan Sentemon <sentemon.i@gmail.com>
2024-09-03 20:20:00 +0200

    Add migration to add ConnectionId field to Users table

---
+++ backend/src/MessagingPlatform.Infrastructure/Migrations/20240903173851_AddFieldConnectionId.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MessagingPlatform.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFieldConnectionId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ConnectionId",
+                table: "Users",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ConnectionId",
+                table: "Users");
+        }
+    }
+}
