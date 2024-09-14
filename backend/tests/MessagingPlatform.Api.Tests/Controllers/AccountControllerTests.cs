df245d8b82a894d15c3108e0c27b11a00e16e5e7
Ivan Sentemon <sentemon.i@gmail.com>
2024-09-14 14:33:10 +0200

    add tests for SignIn method

--- backend/tests/MessagingPlatform.Api.Tests/Controllers/AccountControllerTests.cs
+++ backend/tests/MessagingPlatform.Api.Tests/Controllers/AccountControllerTests.cs
@@ -6,6 +6,7 @@ using MessagingPlatform.Application.Common.Interfaces;
 using MessagingPlatform.Application.Common.Models.UserDTOs;
 using Moq;
 using FluentAssertions;
+using MessagingPlatform.Application.CQRS.Users.Commands.SignIn;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MessagingPlatform.Api.Tests.Controllers;
@@ -71,4 +72,72 @@ public class AccountControllerTests
         badRequestResult.StatusCode.Should().Be(400);
         badRequestResult.Value.Should().Be("Invalid user data.");
     }
+
+    [Fact]
+    public async Task SignIn_ShouldReturnOk_WhenUserIsSignedInSuccessfully()
+    {
+        // Arrange
+        var signInDto = new SignInDto
+        {
+            Username = "qwerty",
+            Password = "Qwerty"
+        };
+
+        var token = "valid-token";
+
+        _mediatrMock
+            .Setup(m => m.Send(It.IsAny<SignInCommand>(), default))
+            .ReturnsAsync(token);
+
+        // Act
+        var result = await _controller.SignIn(signInDto);
+
+        // Assert
+        var okResult = result.Should().BeOfType<OkObjectResult>().Subject;
+        okResult.StatusCode.Should().Be(200);
+        okResult.Value.Should().BeEquivalentTo(new { message = "You signed in successfully." });
+        
+        _cookieServiceMock.Verify(c => c.Append("token", token), Times.Once);
+    }
+
+    [Fact]
+    public async Task SignIn_ShouldReturnBadRequest_WhenDtoIsNull()
+    {
+        // Arrange
+        SignInDto? nullSignInDto = null;
+
+        // Act
+        var result = await _controller.SignIn(nullSignInDto);
+
+        // Assert
+        var badRequestResult = result.Should().BeOfType<BadRequestObjectResult>().Subject;
+        badRequestResult.StatusCode.Should().Be(400);
+        badRequestResult.Value.Should().Be("Invalid user data.");
+    }
+    
+    [Fact]
+    public async Task SignIn_ShouldReturnUnauthorized_WhenSignInFails()
+    {
+        // Arrange
+        var wrongSignInDto = new SignInDto
+        {
+            Username = "wrong_user",
+            Password = "wrong_password&*09@@@"
+        };
+
+        string? token = null;
+
+        _mediatrMock
+            .Setup(m => m.Send(It.IsAny<SignInCommand>(), default))
+            .ReturnsAsync(token);
+        
+        // Act
+        var result = await _controller.SignIn(wrongSignInDto);
+        
+        // Assert
+        var unauthorizedResult = result.Should().BeOfType<UnauthorizedObjectResult>().Subject;
+        unauthorizedResult.StatusCode.Should().Be(401);
+        unauthorizedResult.Value.Should().Be("Invalid username or password.");
+    }
+    
 }
