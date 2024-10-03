8070a1dc9d1f3170e10303de43092cf43d468d78
Ivan Sentemon <sentemon.i@gmail.com>
2024-10-03 19:29:56 +0200

    delete useless classes

--- backend/src/MessagingPlatform.Application/CQRS/Messages/Queries/GetMessagesByUserIdAndChatId/GetMessagesByUserIdAndChatIdQuery.cs
+++
@@ -1,7 +0,0 @@
-using MediatR;
-using MessagingPlatform.Application.Common.Models.MessageDTOs;
-using MessagingPlatform.Domain.Entities;
-
-namespace MessagingPlatform.Application.CQRS.Messages.Queries.GetMessagesByUserIdAndChatId;
-
-public record GetMessagesByUserIdAndChatIdQuery(Guid UserId, Guid ChatId) : IRequest<IQueryable<Message>>;
